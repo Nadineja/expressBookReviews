040ee2c6141474e3822a8db72838476d01233b4d
Nadineja <jammetnadine@gmail.com>
2023-01-04 20:59:12 -0500

    Minor updates

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -32,9 +32,11 @@ regd_users.post("/login", (req,res) => {
   //Write your code here
   const username = req.body.username;
   const password = req.body.password;
-
+  if (!username || !password) {
+    return res.status(404).json({message: "Error logging in"});
+}
   if (authenticatedUser(username, password)) {
-      let accessToken = jwt.sign({data:password}, "access", {expiresIn: 3600});
+      let accessToken = jwt.sign({data:password}, "access", {expiresIn: 60*60});
       req.session.authorization = {accessToken,username};
       return res.status(200).send("User successfully logged in");
   }
